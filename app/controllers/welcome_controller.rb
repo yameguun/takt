36cbadfb9b29e157fa5c95eb03a10a03b6c64792
Seiji Tanaka <nsy999jp@gmail.com>
2025-08-06 17:44:47 +0900

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < BaseController
+
   def index
+    @write_date = params[:report_date] || Date.today.strftime("%Y-%m-%d")
+    @daily_report = current_user.daily_reports.find_or_initialize_by(date: @write_date)
   end
 end
