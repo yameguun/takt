7acdee5dfae35ad9bb66bfc8e08a0ec129c0496b
Seiji Tanaka <nsy999jp@gmail.com>
2025-08-06 18:06:03 +0900

    Fix

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -3,6 +3,6 @@ class Api::ClientsController < ApplicationController
 
   def index
     @clients = Client.where(company_id: params[:company_id])
-    @clients = @clients.where("name LIKE ?", "%#{params[:name]}%") if params[:name].present?
+    @clients = @clients.where("name LIKE ? OR kana LIKE ?", "%#{params[:name]}%","%#{params[:name]}%") if params[:name].present?
   end
 end
