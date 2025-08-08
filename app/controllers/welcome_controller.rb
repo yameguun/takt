6ced8d656409f7c9b4f7fe38dc97fa70e3e2fef6
Seiji Tanaka <nsy999jp@gmail.com>
2025-08-08 14:40:04 +0900

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < BaseController
 
   def index
     @write_date = params[:report_date] || Date.today.strftime("%Y-%m-%d")
-    @daily_report = current_user.daily_reports.find_or_initialize_by(date: @write_date)
+    @daily_report = current_user.daily_reports.find_or_create_by(date: @write_date)
   end
 end
