bbbd6a6f6bf45f25f52a7ec71de5bd72086e4a22
yameguun <nsy999jp@gmail.com>
2025-08-09 15:38:47 +0900

    Fix

--- public/main/js/inspinia.js
+++ public/main/js/inspinia.js
@@ -1,26 +1,24 @@
 /*
  *
  *   INSPINIA - Responsive Admin Theme
- *   version 2.4
+ *   version 2.4 (jQuery 3.7.1 対応)
  *
  */
 
-
-$(document).ready(function () {
-
+$(function () {
 
     // Add body-small class if window less than 768px
-    if ($(this).width() < 769) {
-        $('body').addClass('body-small')
+    if ($(window).width() < 769) {
+        $('body').addClass('body-small');
     } else {
-        $('body').removeClass('body-small')
+        $('body').removeClass('body-small');
     }
 
     // MetsiMenu
     $('#side-menu').metisMenu();
 
     // Collapse ibox function
-    $('.collapse-link').click(function () {
+    $('.collapse-link').on('click', function () {
         var ibox = $(this).closest('div.ibox');
         var button = $(this).find('i');
         var content = ibox.find('div.ibox-content');
@@ -28,19 +26,19 @@ $(document).ready(function () {
         button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
         ibox.toggleClass('').toggleClass('border-bottom');
         setTimeout(function () {
-            ibox.resize();
-            ibox.find('[id^=map-]').resize();
+            ibox.trigger('resize');
+            ibox.find('[id^=map-]').trigger('resize');
         }, 50);
     });
 
     // Close ibox function
-    $('.close-link').click(function () {
+    $('.close-link').on('click', function () {
         var content = $(this).closest('div.ibox');
         content.remove();
     });
 
     // Fullscreen ibox function
-    $('.fullscreen-link').click(function () {
+    $('.fullscreen-link').on('click', function () {
         var ibox = $(this).closest('div.ibox');
         var button = $(this).find('i');
         $('body').toggleClass('fullscreen-ibox-mode');
@@ -52,8 +50,8 @@ $(document).ready(function () {
     });
 
     // Close menu in canvas mode
-    $('.close-canvas-menu').click(function () {
-        $("body").toggleClass("mini-navbar");
+    $('.close-canvas-menu').on('click', function () {
+        $('body').toggleClass('mini-navbar');
         SmoothlyMenu();
     });
 
@@ -64,7 +62,7 @@ $(document).ready(function () {
     });
 
     // Open close right sidebar
-    $('.right-sidebar-toggle').click(function () {
+    $('.right-sidebar-toggle').on('click', function () {
         $('#right-sidebar').toggleClass('sidebar-open');
     });
 
@@ -76,7 +74,7 @@ $(document).ready(function () {
     });
 
     // Open close small chat
-    $('.open-small-chat').click(function () {
+    $('.open-small-chat').on('click', function () {
         $(this).children().toggleClass('fa-comments').toggleClass('fa-remove');
         $('.small-chat-box').toggleClass('active');
     });
@@ -88,7 +86,7 @@ $(document).ready(function () {
     });
 
     // Small todo handler
-    $('.check-link').click(function () {
+    $('.check-link').on('click', function () {
         var button = $(this).find('i');
         var label = $(this).next('span');
         button.toggleClass('fa-check-square').toggleClass('fa-square-o');
@@ -96,61 +94,52 @@ $(document).ready(function () {
         return false;
     });
 
-    // Append config box / Only for demo purpose
-    // Uncomment on server mode to enable XHR calls
-    //$.get("skin-config.html", function (data) {
-    //    if (!$('body').hasClass('no-skin-config'))
-    //        $('body').append(data);
-    //});
-
     // Minimalize menu
-    $('.navbar-minimalize').click(function () {
-        $("body").toggleClass("mini-navbar");
+    $('.navbar-minimalize').on('click', function () {
+        $('body').toggleClass('mini-navbar');
         SmoothlyMenu();
-
     });
 
     // Tooltips demo
     $('.tooltip-demo').tooltip({
-        selector: "[data-toggle=tooltip]",
-        container: "body"
+        selector: '[data-toggle=tooltip]',
+        container: 'body'
     });
 
     // Move modal to body
-    // Fix Bootstrap backdrop issu with animation.css
-    $('.modal').appendTo("body");
+    // Fix Bootstrap backdrop issue with animation.css
+    $('.modal').appendTo('body');
 
     // Full height of sidebar
     function fix_height() {
-        var heightWithoutNavbar = $("body > #wrapper").height() - 61;
-        $(".sidebard-panel").css("min-height", heightWithoutNavbar + "px");
+        var heightWithoutNavbar = $('body > #wrapper').height() - 61;
+        $('.sidebard-panel').css('min-height', heightWithoutNavbar + 'px');
 
         var navbarHeigh = $('nav.navbar-default').height();
         var wrapperHeigh = $('#page-wrapper').height();
 
         if (navbarHeigh > wrapperHeigh) {
-            $('#page-wrapper').css("min-height", navbarHeigh + "px");
+            $('#page-wrapper').css('min-height', navbarHeigh + 'px');
         }
 
         if (navbarHeigh < wrapperHeigh) {
-            $('#page-wrapper').css("min-height", $(window).height() + "px");
+            $('#page-wrapper').css('min-height', $(window).height() + 'px');
         }
 
         if ($('body').hasClass('fixed-nav')) {
             if (navbarHeigh > wrapperHeigh) {
-                $('#page-wrapper').css("min-height", navbarHeigh - 60 + "px");
+                $('#page-wrapper').css('min-height', (navbarHeigh - 60) + 'px');
             } else {
-                $('#page-wrapper').css("min-height", $(window).height() - 60 + "px");
+                $('#page-wrapper').css('min-height', ($(window).height() - 60) + 'px');
             }
         }
-
     }
 
     fix_height();
 
     // Fixed Sidebar
-    $(window).bind("load", function () {
-        if ($("body").hasClass('fixed-sidebar')) {
+    $(window).on('load', function () {
+        if ($('body').hasClass('fixed-sidebar')) {
             $('.sidebar-collapse').slimScroll({
                 height: '100%',
                 railOpacity: 0.9
@@ -159,7 +148,7 @@ $(document).ready(function () {
     });
 
     // Move right sidebar top after scroll
-    $(window).scroll(function () {
+    $(window).on('scroll', function () {
         if ($(window).scrollTop() > 0 && !$('body').hasClass('fixed-nav')) {
             $('#right-sidebar').addClass('sidebar-top');
         } else {
@@ -167,83 +156,80 @@ $(document).ready(function () {
         }
     });
 
-    $(window).bind("load resize scroll", function () {
-        if (!$("body").hasClass('body-small')) {
+    $(window).on('load resize scroll', function () {
+        if (!$('body').hasClass('body-small')) {
             fix_height();
         }
     });
 
-    $("[data-toggle=popover]")
-        .popover();
+    //$('[data-toggle=popover]').popover();
 
     // Add slimscroll to element
-    $('.full-height-scroll').slimscroll({
+    $('.full-height-scroll').slimScroll({
         height: '100%'
-    })
+    });
 });
 
-
 // Minimalize menu when screen is less than 768px
-$(window).bind("resize", function () {
-    if ($(this).width() < 769) {
-        $('body').addClass('body-small')
+$(window).on('resize', function () {
+    if ($(window).width() < 769) {
+        $('body').addClass('body-small');
     } else {
-        $('body').removeClass('body-small')
+        $('body').removeClass('body-small');
     }
 });
 
 // Local Storage functions
 // Set proper body class and plugins based on user configuration
-// $(document).ready(function () {
-//     if (localStorageSupport) {
+$(function () {
+    if (localStorageSupport()) {
 
-//         var collapse = localStorage.getItem("collapse_menu");
-//         var fixedsidebar = localStorage.getItem("fixedsidebar");
-//         var fixednavbar = localStorage.getItem("fixednavbar");
-//         var boxedlayout = localStorage.getItem("boxedlayout");
-//         var fixedfooter = localStorage.getItem("fixedfooter");
+        var collapse = localStorage.getItem('collapse_menu');
+        var fixedsidebar = localStorage.getItem('fixedsidebar');
+        var fixednavbar = localStorage.getItem('fixednavbar');
+        var boxedlayout = localStorage.getItem('boxedlayout');
+        var fixedfooter = localStorage.getItem('fixedfooter');
 
-//         var body = $('body');
+        var body = $('body');
 
-//         if (fixedsidebar == 'on') {
-//             body.addClass('fixed-sidebar');
-//             $('.sidebar-collapse').slimScroll({
-//                 height: '100%',
-//                 railOpacity: 0.9
-//             });
-//         }
+        if (fixedsidebar === 'on') {
+            body.addClass('fixed-sidebar');
+            $('.sidebar-collapse').slimScroll({
+                height: '100%',
+                railOpacity: 0.9
+            });
+        }
 
-//         if (collapse == 'on') {
-//             if (body.hasClass('fixed-sidebar')) {
-//                 if (!body.hasClass('body-small')) {
-//                     body.addClass('mini-navbar');
-//                 }
-//             } else {
-//                 if (!body.hasClass('body-small')) {
-//                     body.addClass('mini-navbar');
-//                 }
+        if (collapse === 'on') {
+            if (body.hasClass('fixed-sidebar')) {
+                if (!body.hasClass('body-small')) {
+                    body.addClass('mini-navbar');
+                }
+            } else {
+                if (!body.hasClass('body-small')) {
+                    body.addClass('mini-navbar');
+                }
+            }
+        }
 
-//             }
-//         }
+        if (fixednavbar === 'on') {
+            $('.navbar-static-top').removeClass('navbar-static-top').addClass('navbar-fixed-top');
+            body.addClass('fixed-nav');
+        }
 
-//         if (fixednavbar == 'on') {
-//             $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
-//             body.addClass('fixed-nav');
-//         }
+        if (boxedlayout === 'on') {
+            body.addClass('boxed-layout');
+        }
 
-//         if (boxedlayout == 'on') {
-//             body.addClass('boxed-layout');
-//         }
-
-//         if (fixedfooter == 'on') {
-//             $(".footer").addClass('fixed');
-//         }
-//     }
-// });
+        if (fixedfooter === 'on') {
+            $('.footer').addClass('fixed');
+        }
+    }
+});
 
 // check if browser support HTML5 local storage
 function localStorageSupport() {
-    return (('localStorage' in window) && window['localStorage'] !== null)
+    return (('localStorage' in window) && window.localStorage !== null);
 }
 
 // For demo purpose - animation css script
@@ -254,11 +240,12 @@ function animationHover(element, animation) {
             element.addClass('animated ' + animation);
         },
         function () {
-            //wait for animation to finish before removing classes
+            // wait for animation to finish before removing classes
             window.setTimeout(function () {
                 element.removeClass('animated ' + animation);
             }, 2000);
-        });
+        }
+    );
 }
 
 function SmoothlyMenu() {
@@ -266,16 +253,14 @@ function SmoothlyMenu() {
         // Hide menu in order to smoothly turn on when maximize menu
         $('#side-menu').hide();
         // For smoothly turn on menu
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(400);
-            }, 200);
+        setTimeout(function () {
+            $('#side-menu').fadeIn(400);
+        }, 200);
     } else if ($('body').hasClass('fixed-sidebar')) {
         $('#side-menu').hide();
-        setTimeout(
-            function () {
-                $('#side-menu').fadeIn(400);
-            }, 100);
+        setTimeout(function () {
+            $('#side-menu').fadeIn(400);
+        }, 100);
     } else {
         // Remove all inline style from jquery fadeIn function to reset menu state
         $('#side-menu').removeAttr('style');
@@ -284,18 +269,14 @@ function SmoothlyMenu() {
 
 // Dragable panels
 function WinMove() {
-    var element = "[class*=col]";
-    var handle = ".ibox-title";
-    var connect = "[class*=col]";
-    $(element).sortable(
-        {
-            handle: handle,
-            connectWith: connect,
-            tolerance: 'pointer',
-            forcePlaceholderSize: true,
-            opacity: 0.8
-        })
-        .disableSelection();
+    var element = '[class*=col]';
+    var handle = '.ibox-title';
+    var connect = '[class*=col]';
+    $(element).sortable({
+        handle: handle,
+        connectWith: connect,
+        tolerance: 'pointer',
+        forcePlaceholderSize: true,
+        opacity: 0.8
+    }).disableSelection();
 }
-
-
