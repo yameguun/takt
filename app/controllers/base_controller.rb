6e47b05c0c3833019e4ecdd78d2a0dd7de52747f
Seiji Tanaka <nsy999jp@gmail.com>
2025-08-18 10:34:41 +0900

    残業申請の承認バッジ

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,5 +1,15 @@
 class BaseController < ApplicationController
   before_action :require_login
+  before_action :daily_report_project_count
+
+  def daily_report_project_count
+    @daily_report_project_count = DailyReportProject
+      .joins(daily_report: :user)
+      .joins(project: :client)
+      .where(is_overtime_approved: false)
+      .where(is_overtime_requested: true)
+      .where(users: { department_id: current_user.department_id }).count
+  end
 
   private
 
