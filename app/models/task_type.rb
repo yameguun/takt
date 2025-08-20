9cc0dd94181a308772fecb71234ca9f2ebc84200
Seiji Tanaka <nsy999jp@gmail.com>
2025-08-20 12:30:09 +0900

    Fix

--- app/models/task_type.rb
+++ app/models/task_type.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: task_types
+#
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  company_id :bigint           not null
+#
+# Indexes
+#
+#  index_task_types_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
 class TaskType < ApplicationRecord
   belongs_to :company
   validates :name, presence: true, uniqueness: { scope: :company_id }
