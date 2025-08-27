c2683965b3950844992c27df65228834ac25b473
Seiji Tanaka <nsy999jp@gmail.com>
2025-08-27 10:42:21 +0900

    Fix

--- app/controllers/api/clients_controller.rb
+++ app/controllers/api/clients_controller.rb
@@ -3,6 +3,21 @@ class Api::ClientsController < ApplicationController
 
   def index
     @clients = Client.where(company_id: params[:company_id])
-    @clients = @clients.where("name LIKE ? OR kana LIKE ?", "%#{params[:name]}%","%#{params[:name]}%") if params[:name].present?
+
+    if params[:name].present?
+      @clients = @clients.where("name LIKE ? OR kana LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+                         .order('name ASC')
+    else
+      # 空検索時は最近更新されたものを優先表示
+      @clients = @clients.order(updated_at: :desc)
+    end
+
+    # limit パラメータ対応
+    if params[:limit].present?
+      @clients = @clients.limit(params[:limit].to_i)
+    else
+      # デフォルトで制限をかける（大量データ対策）
+      @clients = @clients.limit(10) unless params[:name].present?
+    end
   end
 end
