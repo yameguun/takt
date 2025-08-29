dbb1f463f86a66a30c3bbb8cae5147e72df1e19b
Seiji Tanaka <nsy999jp@gmail.com>
2025-08-29 11:30:01 +0900

    Fix

--- db/fixtures/production/00_administrator.rb
+++ db/fixtures/production/00_administrator.rb
@@ -1,5 +1,5 @@
 # 管理者データの作成
 Administrator.find_or_create_by!(email: 'admin@suncackikaku.co.jp') do |admin|
-  admin.password = 'password'
-  admin.password_confirmation = 'password'
+  admin.password = 'Kj%$USQgAEj6'
+  admin.password_confirmation = 'Kj%$USQgAEj6'
 end
